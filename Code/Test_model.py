62d7ddafb41592dbc05fab21f8ef57b7ab7d2aa2
Phan Nhật Tân <162537750+nhattan2005@users.noreply.github.com>
2024-08-13 21:43:33 +0700

    Add files via upload

---
+++ Code/Test_model.py
@@ -0,0 +1,43 @@
+import os
+import pathlib
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import image
+plt.style.use("fivethirtyeight")
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from tensorflow.keras import layers
+from keras.layers import Dense, Flatten, Rescaling
+
+
+my_xray_cnnmodel = keras.models.load_model("D:/xray_model.h5")
+
+# Defining an image path from the "pred" folder:
+image_path = r"D:\Tan's data\STUDY____________\RapidMiner\lungxray\new\new2.jpg"
+
+# Preprocessing the image to 224x224 size and predicting the label:
+image = tf.keras.preprocessing.image.load_img(image_path, target_size=(224, 224))
+input_arr = tf.keras.preprocessing.image.img_to_array(image)
+input_arr = np.array([input_arr])
+predictions = my_xray_cnnmodel.predict(input_arr)
+
+classes = ['COVID', 'NORMAL']
+
+actual = ''
+
+for class_name in classes:
+    if class_name in image_path:
+        actual = class_name
+
+pred = classes[np.argmax(predictions, axis=1)[0]]
+
+# Finally we are displaying the predicted outcome:
+plt.figure(figsize=[8,5])
+plt.imshow(image, cmap='gray')
+plt.title("Actual:"+actual+" /Predicted:"+pred, size=15)
+plt.axis('off')
+plt.show()
